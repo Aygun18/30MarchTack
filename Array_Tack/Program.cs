e213e0d9e653d2557710e735130c25072d860dd9
Aygun <sahmuradova18@gmail.com>
2023-03-30 00:50:03 +0400

    Array_Tack

---
+++ Array_Tack/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Array_Tack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 3, 4, 5, 6, 7 };
+            InsertArray(ref arr, 1, 2, 3);
+            foreach (var num in arr)
+            {
+                Console.WriteLine(num);
+            }
+        }
+        public static void InsertArray(ref int[] array, params int[] nums)
+        {
+            int[] newArray = new int[array.Length + nums.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                newArray[array.Length + i] = nums[i];
+            }
+            array = newArray;
+        }
+    }
+}
